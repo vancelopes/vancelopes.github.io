71358d88e4706648a70e10ada0085388f0fe1523
Evandro <evandro@screencorp.com.br>
2019-10-31 16:32:12 -0300

    changed App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        It's ALIVE!!!
+      </header>
+    </div>
+  );
+}
+
+export default App;
