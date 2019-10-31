99583be3b73547bb4b6cd9134c61aff97da85d63
Evandro <evandro@screencorp.com.br>
2019-10-31 16:29:55 -0300

    Updates

---
+++ precache-manifest.0e36974b2b76c61ffb0e5237aca9b28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3d33ee793669bc831981426fccc56f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0aa9e3bbaf501cd40302",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "4f9e31d15668e3f49a88",
+    "url": "./static/js/2.35902b64.chunk.js"
+  },
+  {
+    "revision": "0aa9e3bbaf501cd40302",
+    "url": "./static/js/main.cef938f3.chunk.js"
+  },
+  {
+    "revision": "4a70b64655a233c1db74",
+    "url": "./static/js/runtime-main.736efe22.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
