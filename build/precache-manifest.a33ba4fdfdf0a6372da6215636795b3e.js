4b932a94cc9fdb9979a61c9f446f56af9dd07fc8
Evandro <evandro@screencorp.com.br>
2019-10-31 15:02:02 -0300

    first commit

---
+++ build/precache-manifest.a33ba4fdfdf0a6372da6215636795b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1089055c6db7e4e75e0533a703dd312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "347c43988dd22ef58e1f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f9e31d15668e3f49a88",
+    "url": "/static/js/2.35902b64.chunk.js"
+  },
+  {
+    "revision": "347c43988dd22ef58e1f",
+    "url": "/static/js/main.5e4651b2.chunk.js"
+  },
+  {
+    "revision": "703c272d6fad717e1914",
+    "url": "/static/js/runtime-main.73859b0c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
